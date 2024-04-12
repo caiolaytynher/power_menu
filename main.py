66a543baee108cfeff4985e9e8c52da719b4b23e
caiolaytynher <csrlaytynher@proton.me>
2024-04-12 18:18:30 -0300

    removed unecessary variables

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-#!/home/caio/.pyenv/versions/3.12.2/bin/python
-
 import os
 import subprocess
 from enum import StrEnum
@@ -19,29 +17,34 @@ class PowerOffOpt(StrEnum):
 
 
 def main() -> None:
-    theme = {
-        "power_off": str(WORKSPACE / "themes/power_off.rasi"),
-        "confirm": str(WORKSPACE / "themes/confirm.rasi"),
-    }
-
-    power_off_opts = "\n".join(list(PowerOffOpt))
-
-    power_off_prompt = subprocess.run(
-        ["/usr/bin/rofi", "-dmenu", "-theme", theme["power_off"]],
-        input=power_off_opts,
+    power_off_cmd = subprocess.run(
+        [
+            "/usr/bin/rofi",
+            "-dmenu",
+            "-theme",
+            WORKSPACE / "themes/power_off.rasi",
+        ],
+        input="\n".join(list(PowerOffOpt)),
         encoding="utf-8",
         capture_output=True,
     )
 
-    if power_off_prompt.stderr:
+    if power_off_cmd.stderr:
         return
 
-    power_off_opt = power_off_prompt.stdout.strip()
+    power_off_opt = power_off_cmd.stdout.strip()
     if not power_off_opt:
         return
 
     confirm_prompt = subprocess.run(
-        ["/usr/bin/rofi", "-dmenu", "-p", "Are you sure?", "-theme", theme["confirm"]],
+        [
+            "/usr/bin/rofi",
+            "-dmenu",
+            "-p",
+            "Are you sure?",
+            "-theme",
+            WORKSPACE / "themes/confirm.rasi",
+        ],
         input="Yes\nNo",
         encoding="utf-8",
         capture_output=True,
